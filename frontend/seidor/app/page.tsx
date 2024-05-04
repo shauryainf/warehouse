14ac0b2525457479496a5eb3b9adf7726e38c2e6
Harsimran <72358179+Harsimran29120@users.noreply.github.com>
2024-05-04 15:40:05 +0200

    Add files via upload

--- frontend/seidor/app/page.tsx
+++ frontend/seidor/app/page.tsx
@@ -3,7 +3,28 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { VestGPT } from "@/components/home/gpt";
 import Image from "next/image";
+
 export default function Home() {
+  function myFunction() {
+    console.log("Hello World")
+    //navigator.bluetooth.requestDevice({acceptAllDevices: true})
+    
+      try {
+        const device = navigator.bluetooth.requestDevice({
+          acceptAllDevices: true
+        });
+        const connection = device.connect();
+        if (connection) {
+          console.log("Device is connected!");
+        } else {
+          console.log("Connection failed or cancelled by user.");
+        }
+      } catch (error) {
+        console.error("Error connecting to device:", error);
+      }
+    
+    
+  }
   return (
     <div className="flex flex-col h-screen home ">
       
@@ -15,8 +36,10 @@ export default function Home() {
       <div className="flex items-center">
         <h1 className="text-lg font-medium">SEIDOR AI</h1>
       </div>
+      <button onClick={myFunction}><img width={30} src="https://static-00.iconduck.com/assets.00/bluetooth-icon-2048x2048-7i2pzxl7.png" alt="" /></button>
 
     </header>
+          
 
       <div className="m-2 inline-block mx-auto">
         <VestGPT />
